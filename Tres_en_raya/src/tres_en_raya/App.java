7b75185c551eb38456cc7931b097f3feabe3dde1
Francisco Garcia Gomez <frangargo.trabajo@gmail.com>
2023-12-20 11:57:38 +0100

    Creacion de combinaciones de ganar
    Se ha creado las combinaciones para ganar ademas de realizar cambios en el fin del juego

--- Tres_en_raya/src/tres_en_raya/App.java
+++ Tres_en_raya/src/tres_en_raya/App.java
@@ -5,82 +5,81 @@ import java.util.Scanner;
 
 public class App {
 
-	//Este es el juego del 3 en raya, muy simple
-	//Primero haremos lo simple, colocar todas las piezas posibles hasta que no se puedan poner mas
-	//Mas tarde hare lo de quitar la piza que quieras hasta poder ganar (solo tendrás 3 piezas claro )
-	//Mas tarde hare un menu para que jueges con alguien o contra la maquina
-	//Primero jugaremos contra alguien
-	
+	// Este es el juego del 3 en raya, muy simple
+	// Primero haremos lo simple, colocar todas las piezas posibles hasta que no se
+	// puedan poner mas
+	// Mas tarde hare lo de quitar la piza que quieras hasta poder ganar (solo
+	// tendrás 3 piezas claro )
+	// Mas tarde hare un menu para que jueges con alguien o contra la maquina
+	// Primero jugaremos contra alguien
+
 	private static Scanner sc = new Scanner(System.in);
-	
+
 	public static void main(String[] args) {
 
-		//Creacion del tablero de juego!
-		//Se jugará con letras X y O
-		//Donde no haya nada serán .
-		
+		// Creacion del tablero de juego!
+		// Se jugará con letras X y O
+		// Donde no haya nada serán .
+
 		menu();
-		
 
 	}
-	
+
 	public static void menu() {
-		String [][] tablero = crearTablero();
+		String[][] tablero = crearTablero();
 		mostrarTablero(tablero);
-		//tablero = colocarPiezaJ1(tablero);
-		//mostrarTablero(tablero);
+		// tablero = colocarPiezaJ1(tablero);
+		// mostrarTablero(tablero);
 		juego(tablero);
 	}
-	
+
 	public static String[][] crearTablero() {
 		String[][] tablero = new String[3][3];
-		
+
 		for (int i = 0; i < tablero.length; i++) {
 			for (int j = 0; j < tablero[i].length; j++) {
 				tablero[i][j] = ".";
 			}
 		}
-		
+
 		return tablero;
 	}
-	
-	public static String[][] mostrarTablero(String[][] tablero){
-		
-		
+
+	public static String[][] mostrarTablero(String[][] tablero) {
+
 		for (int i = 0; i < tablero.length; i++) {
 			for (int j = 0; j < tablero[i].length; j++) {
 				System.out.print(tablero[i][j] + " ");
 			}
 			System.out.println("");
 		}
-		
+
 		return tablero;
 	}
-	
-	public static String[][] colocarPiezaJ1(String[][] tablero){
-		
+
+	public static String[][] colocarPiezaJ1(String[][] tablero) {
+
 		int posicionUno = 0;
 		int posicionDos = 0;
-		
+
 		System.out.println("Jugador 1 coloque su pieza");
 		do {
-		System.out.println("Diga las coordenadas de la fila (0 a 2 tanto fila y columna)");
-		try {
-			posicionUno = sc.nextInt();
-			if (posicionUno > 2) {
-				System.out.println("Recueda el maximo es 2 y el minimo 0");
+			System.out.println("Diga las coordenadas de la fila (0 a 2 tanto fila y columna)");
+			try {
+				posicionUno = sc.nextInt();
+				if (posicionUno > 2) {
+					System.out.println("Recueda el maximo es 2 y el minimo 0");
+					posicionUno = -1;
+				} else if (posicionUno < 0) {
+					System.out.println("Recueda el maximo es 2 y el minimo 0");
+				}
+			} catch (Exception e) {
+				System.out.println("Recueda que estamos jugando con numeros");
 				posicionUno = -1;
+				sc.next();
 			}
-			else if (posicionUno < 0) {
-				System.out.println("Recueda el maximo es 2 y el minimo 0");
-			}
-		} catch (Exception e) {
-			System.out.println("Recueda que estamos jugando con numeros");
-			posicionUno = -1;
-			sc.next();
-		}
-		}while(posicionUno == -1);
-		
+		} while (posicionUno == -1);
+
 		do {
 			System.out.println("Diga las coordenadas de la columna (0 a 2 tanto fila y columna)");
 			try {
@@ -88,8 +87,7 @@ public class App {
 				if (posicionDos > 2) {
 					System.out.println("Recueda el maximo es 2 y el minimo 0");
 					posicionDos = -1;
-				}
-				else if (posicionDos < 0) {
+				} else if (posicionDos < 0) {
 					System.out.println("Recueda el maximo es 2 y el minimo 0");
 				}
 			} catch (Exception e) {
@@ -97,75 +95,71 @@ public class App {
 				posicionDos = -1;
 				sc.next();
 			}
-			}while(posicionUno == -1);
+		} while (posicionUno == -1);
 
-		
 		for (int i = 0; i < tablero.length; i++) {
 			for (int j = 0; j < tablero[i].length; j++) {
-				while(tablero[posicionUno][posicionDos] == "O") {
+				while (tablero[posicionUno][posicionDos] == "O") {
 					System.out.println("Ahi no puedes poner, ya hay una pieza");
-						System.out.println("Diga las coordenadas de la fila (0 a 2 tanto fila y columna)");
-						try {
-							posicionUno = sc.nextInt();
-							if (posicionUno > 2) {
-								System.out.println("Recueda el maximo es 2 y el minimo 0");
-								posicionUno = -1;
-							}
-							else if (posicionUno < 0) {
-								System.out.println("Recueda el maximo es 2 y el minimo 0");
-							}
-						} catch (Exception e) {
-							System.out.println("Recueda que estamos jugando con numeros");
+					System.out.println("Diga las coordenadas de la fila (0 a 2 tanto fila y columna)");
+					try {
+						posicionUno = sc.nextInt();
+						if (posicionUno > 2) {
+							System.out.println("Recueda el maximo es 2 y el minimo 0");
 							posicionUno = -1;
-							sc.next();
+						} else if (posicionUno < 0) {
+							System.out.println("Recueda el maximo es 2 y el minimo 0");
 						}
-							System.out.println("Diga las coordenadas de la columna (0 a 2 tanto fila y columna)");
-							try {
-								posicionDos = sc.nextInt();
-								if (posicionDos > 2) {
-									System.out.println("Recueda el maximo es 2 y el minimo 0");
-									posicionDos = -1;
-								}
-								else if (posicionDos < 0) {
-									System.out.println("Recueda el maximo es 2 y el minimo 0");
-								}
-							} catch (Exception e) {
-								System.out.println("Recueda que estamos jugando con numeros");
-								posicionDos = -1;
-								sc.next();
-							}
+					} catch (Exception e) {
+						System.out.println("Recueda que estamos jugando con numeros");
+						posicionUno = -1;
+						sc.next();
+					}
+					System.out.println("Diga las coordenadas de la columna (0 a 2 tanto fila y columna)");
+					try {
+						posicionDos = sc.nextInt();
+						if (posicionDos > 2) {
+							System.out.println("Recueda el maximo es 2 y el minimo 0");
+							posicionDos = -1;
+						} else if (posicionDos < 0) {
+							System.out.println("Recueda el maximo es 2 y el minimo 0");
+						}
+					} catch (Exception e) {
+						System.out.println("Recueda que estamos jugando con numeros");
+						posicionDos = -1;
+						sc.next();
+					}
 				}
 				tablero[posicionUno][posicionDos] = "X";
 			}
 		}
-		
+
 		return tablero;
 	}
-	
-public static String[][] colocarPiezaJ2(String[][] tablero){
-		
+
+	public static String[][] colocarPiezaJ2(String[][] tablero) {
+
 		int posicionUno = 0;
 		int posicionDos = 0;
-		
+
 		System.out.println("Jugador 2 coloque su pieza");
 		do {
-		System.out.println("Diga las coordenadas de la fila (0 a 2 tanto fila y columna)");
-		try {
-			posicionUno = sc.nextInt();
-			if (posicionUno > 2) {
-				System.out.println("Recueda el maximo es 2 y el minimo 0");
+			System.out.println("Diga las coordenadas de la fila (0 a 2 tanto fila y columna)");
+			try {
+				posicionUno = sc.nextInt();
+				if (posicionUno > 2) {
+					System.out.println("Recueda el maximo es 2 y el minimo 0");
+					posicionUno = -1;
+				} else if (posicionUno < 0) {
+					System.out.println("Recueda el maximo es 2 y el minimo 0");
+				}
+			} catch (Exception e) {
+				System.out.println("Recueda que estamos jugando con numeros");
 				posicionUno = -1;
+				sc.next();
 			}
-			else if (posicionUno < 0) {
-				System.out.println("Recueda el maximo es 2 y el minimo 0");
-			}
-		} catch (Exception e) {
-			System.out.println("Recueda que estamos jugando con numeros");
-			posicionUno = -1;
-			sc.next();
-		}
-		}while(posicionUno == -1);
-		
+		} while (posicionUno == -1);
+
 		do {
 			System.out.println("Diga las coordenadas de la columna (0 a 2 tanto fila y columna)");
 			try {
@@ -173,8 +167,7 @@ public static String[][] colocarPiezaJ2(String[][] tablero){
 				if (posicionDos > 2) {
 					System.out.println("Recueda el maximo es 2 y el minimo 0");
 					posicionDos = -1;
-				}
-				else if (posicionDos < 0) {
+				} else if (posicionDos < 0) {
 					System.out.println("Recueda el maximo es 2 y el minimo 0");
 				}
 			} catch (Exception e) {
@@ -182,86 +175,205 @@ public static String[][] colocarPiezaJ2(String[][] tablero){
 				posicionDos = -1;
 				sc.next();
 			}
-			}while(posicionUno == -1);
+		} while (posicionUno == -1);
 
-		
 		for (int i = 0; i < tablero.length; i++) {
 			for (int j = 0; j < tablero[i].length; j++) {
-				while(tablero[posicionUno][posicionDos] == "X") {
+				while (tablero[posicionUno][posicionDos] == "X") {
 					System.out.println("Ahi no puedes poner, ya hay una pieza");
-						System.out.println("Diga las coordenadas de la fila (0 a 2 tanto fila y columna)");
-						try {
-							posicionUno = sc.nextInt();
-							if (posicionUno > 2) {
-								System.out.println("Recueda el maximo es 2 y el minimo 0");
-								posicionUno = -1;
-							}
-							else if (posicionUno < 0) {
-								System.out.println("Recueda el maximo es 2 y el minimo 0");
-							}
-						} catch (Exception e) {
-							System.out.println("Recueda que estamos jugando con numeros");
+					System.out.println("Diga las coordenadas de la fila (0 a 2 tanto fila y columna)");
+					try {
+						posicionUno = sc.nextInt();
+						if (posicionUno > 2) {
+							System.out.println("Recueda el maximo es 2 y el minimo 0");
 							posicionUno = -1;
-							sc.next();
+						} else if (posicionUno < 0) {
+							System.out.println("Recueda el maximo es 2 y el minimo 0");
 						}
-							System.out.println("Diga las coordenadas de la columna (0 a 2 tanto fila y columna)");
-							try {
-								posicionDos = sc.nextInt();
-								if (posicionDos > 2) {
-									System.out.println("Recueda el maximo es 2 y el minimo 0");
-									posicionDos = -1;
-								}
-								else if (posicionDos < 0) {
-									System.out.println("Recueda el maximo es 2 y el minimo 0");
-								}
-							} catch (Exception e) {
-								System.out.println("Recueda que estamos jugando con numeros");
-								posicionDos = -1;
-								sc.next();
-							}
+					} catch (Exception e) {
+						System.out.println("Recueda que estamos jugando con numeros");
+						posicionUno = -1;
+						sc.next();
+					}
+					System.out.println("Diga las coordenadas de la columna (0 a 2 tanto fila y columna)");
+					try {
+						posicionDos = sc.nextInt();
+						if (posicionDos > 2) {
+							System.out.println("Recueda el maximo es 2 y el minimo 0");
+							posicionDos = -1;
+						} else if (posicionDos < 0) {
+							System.out.println("Recueda el maximo es 2 y el minimo 0");
+						}
+					} catch (Exception e) {
+						System.out.println("Recueda que estamos jugando con numeros");
+						posicionDos = -1;
+						sc.next();
+					}
 				}
 				tablero[posicionUno][posicionDos] = "O";
 			}
 		}
-		
+
 		return tablero;
 	}
 
 	public static boolean comprobarFin(String[][] tablero) {
-		//Si han acabado devuelve True si no False
+		// Si han acabado devuelve True si no False
 		boolean ret = false;
 		int cont = 0;
 		for (int i = 0; i < tablero.length; i++) {
 			for (int j = 0; j < tablero[i].length; j++) {
-				if(tablero[i][j] == ".") {
+				if (tablero[i][j] == ".") {
 					cont++;
 				}
 			}
 		}
-		if(cont > 0) {
+		if (cont > 0) {
 			ret = false;
-		} else if (cont == 0){
+		} else if (cont == 0) {
 			ret = true;
 		}
 		cont = 0;
 		return ret;
 	}
-	
-	public static void juego(String[][] tablero) {
-		
-		boolean fin = comprobarFin(tablero);
-		
-		while(fin == false) {
-			//Comienza el jugador 1
-			//Seguido el jugador 2
-			tablero = colocarPiezaJ1(tablero);
-			mostrarTablero(tablero);
-			fin = comprobarFin(tablero);
-			tablero = colocarPiezaJ2(tablero);
-			mostrarTablero(tablero);
-			fin = comprobarFin(tablero);
+
+	public static boolean comprobarGanador(String[][] tablero) {
+		int cont = 0;
+		boolean ganar = false;
+
+		// JUGADOR 1
+		// Primero lineas horizontales
+		for (int i = 0; i < tablero.length; i++) {
+			for (int j = 0; j < tablero[i].length; j++) {
+				if (tablero[i][j] == "X") {
+					cont++;
+				}
+				if (cont == 3) {
+					System.out.println("Jugador 1 gana");
+					ganar = true;
+				}
+			}
+			cont = 0;
+
 		}
-		System.out.println("Fin");
+		// Verticales
+		for (int i = 0; i < tablero.length; i++) {
+			for (int j = 0; j < tablero[i].length; j++) {
+				if (tablero[j][i] == "X") {
+					cont++;
+				}
+				if (cont == 3) {
+					System.out.println("Jugador 1 gana");
+					ganar = true;
+				}
+			}
+			cont = 0;
+
+		}
+		// Diagonal desde izquierda
+		for (int i = 0; i < tablero.length; i++) {
+			if (tablero[i][i] == "X") {
+				cont++;
+			}
+			if (cont == 3) {
+				System.out.println("Jugador 1 gana");
+				ganar = true;
+			}
+			cont = 0;
+		}
+		// Diagonal desde derecha
+		for (int i = 2; i > 0; i--) {
+			if (tablero[i][i] == "X") {
+				cont++;
+			}
+			if (cont == 3) {
+				System.out.println("Jugador 1 gana");
+				ganar = true;
+			}
+			cont = 0;
+		}
+		
+		//JUGADOR 2
+		//HORIZONTAL
+		for (int i = 0; i < tablero.length; i++) {
+			for (int j = 0; j < tablero[i].length; j++) {
+				if (tablero[i][j] == "O") {
+					cont++;
+				}
+				if (cont == 3) {
+					System.out.println("Jugador 2 gana");
+					ganar = true;
+				}
+			}
+			cont = 0;
+
+		}
+		// Verticales
+		for (int i = 0; i < tablero.length; i++) {
+			for (int j = 0; j < tablero[i].length; j++) {
+				if (tablero[j][i] == "O") {
+					cont++;
+				}
+				if (cont == 3) {
+					System.out.println("Jugador 2 gana");
+					ganar = true;
+				}
+			}
+			cont = 0;
+
+		}
+		// Diagonal desde izquierda
+		for (int i = 0; i < tablero.length; i++) {
+			if (tablero[i][i] == "O") {
+				cont++;
+			}
+			if (cont == 3) {
+				System.out.println("Jugador 2 gana");
+				ganar = true;
+			}
+			cont = 0;
+		}
+		// Diagonal desde derecha
+		for (int i = 2; i > 0; i--) {
+			if (tablero[i][i] == "O") {
+				cont++;
+			}
+			if (cont == 3) {
+				System.out.println("Jugador 2 gana");
+				ganar = true;
+			}
+			cont = 0;
+		}
+		return ganar;
+	}
+
+	public static void juego(String[][] tablero) {
+
+		boolean fin = comprobarFin(tablero);
+		boolean ganador = comprobarGanador(tablero);
+
+		while (fin == false || ganador == false) {
+			fin = true;
+			while (ganador == false) {
+				ganador = true;
+				// Comienza el jugador 1
+				// Seguido el jugador 2
+				tablero = colocarPiezaJ1(tablero);
+				mostrarTablero(tablero);
+				ganador = comprobarGanador(tablero);
+				fin = comprobarFin(tablero);
+				if (fin == false && ganador == false) {
+					tablero = colocarPiezaJ2(tablero);
+					mostrarTablero(tablero);
+					ganador = comprobarGanador(tablero);
+					fin = comprobarFin(tablero);
+				}
+			}
+
+		}
+		System.out.println("*******************************************");
+		System.out.println("Se ha acabado el juego");
+		System.out.println("*******************************************");
 	}
 
 }
