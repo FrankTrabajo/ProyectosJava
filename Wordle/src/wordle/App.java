fd0c7a29c168a82291f7d7e07a5cbe6707a68d57
Francisco Garcia Gomez <152178489+FrankTrabajo@users.noreply.github.com>
2024-01-02 15:38:14 +0100

    Update App.java

--- Wordle/src/wordle/App.java
+++ Wordle/src/wordle/App.java
@@ -3,11 +3,13 @@ package wordle;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import javax.swing.plaf.FontUIResource;
+
 public class App {
 
 	private static Scanner sc = new Scanner(System.in);
 	private static ArrayList<String> diccionario = new ArrayList<String>();
-	private static String[] tableroJugador = new String[5];
+	private static char[] tableroJugador = new char[5];
 	
 	public static void main(String[] args) {
 		
@@ -29,29 +31,53 @@ public class App {
 		crearTablero();//Lo que hace esto es rellenar el tablero con _ 
 		//Ahora tenemos que coger "la palabra del dia", para ello crearemos un random de logintud
 		//La lista del diccionario y de minimo 1
+		char[] palabraJuego = palabraDelDia();
 		
 		
 		
 	}
 	
-	public static String[] crearTablero() {
+	public static char[] crearTablero() {
 	
 		for (int i = 0; i < tableroJugador.length; i++) {
-			tableroJugador[i] = "_";
+			tableroJugador[i] = '_';
 		}
 		
 		return tableroJugador;
 	}
 	
-	public static String[] palabraDelDia() {
-		String[] palabraDelDia = new String[5];
+	public static char[] palabraDelDia() {
+		char[] palabraDelDia = new char[5];
 		int numeroAleatorio = (int) (Math.random()*diccionario.size()+1);
 		//Hay que sacar una palabra de la lista
 		
 		String palabra = diccionario.get(numeroAleatorio);
 		
-		palabraDelDia = palabra.split("");
-
+		for (int i = 0; i < palabraDelDia.length; i++) {
+			char letra = palabra.charAt(i);
+			palabraDelDia[i] = letra;
+		}
+		
 		return palabraDelDia;
+	}
+	
+	public static char[] comprobarPalabra(char[] palabraDelDia) {
+		
+		System.out.println("Escribe la palabra del dia");
+		String palabra = sc.next();
+		
+		char[] palabraJugador = new char[5];
+		for (int i = 0; i < palabraJugador.length; i++) {
+			char letra = palabra.charAt(i);
+			palabraJugador[i] = letra;
+		}
+		
+		for (int i = 0; i < palabraJugador.length; i++) {
+			if(palabraJugador[i] == palabraDelDia[i]) {
+				tableroJugador[i] = palabraJugador[i];
+			}
+		}
+		
+		return tableroJugador;
 	}
 }
