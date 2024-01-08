b0215790bc07dca5770279089b75effe444629b8
Francisco Garcia Gomez <152178489+FrankTrabajo@users.noreply.github.com>
2024-01-08 16:05:04 +0100

    Update Hobbit.java

--- JuegoRol/src/elSeniorDelLosAnillos/Hobbit.java
+++ JuegoRol/src/elSeniorDelLosAnillos/Hobbit.java
@@ -1,5 +1,16 @@
 package elSeniorDelLosAnillos;
 
-public class Hobbit {
+public class Hobbit extends Heroes {
+
+	public Hobbit(String nombre, int vida, int armadura) {
+		super(nombre, vida, armadura);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void presentacion() {
+		System.out.println("Soy un hobbit");
+	}
 
 }
+
